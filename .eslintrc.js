dda695d4fcd138464ada96702a2cd70f6b4ca4a4
KudravcevPavel <kudravcev92@gmail.com>
2020-10-19 22:20:35 +0300

    dom stucture browserTree

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+export const env = {
+  browser: true,
+  es2020: true,
+};
+export const extends = ["airbnb-base", "prettier"];
+export const parserOptions = {
+  ecmaVersion: 11,
+  sourceType: "module",
+};
+export const rules = {};
